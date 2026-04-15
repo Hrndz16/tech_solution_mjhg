c71fdb69b33b2830a5743953377102e2aa5f4625
hrndz16 <marlonhergom70@gmail.com>
2026-04-15 07:16:31 -0500

    primer commit

---
+++ src/main/java/com/epw/tech_solution_mjhg/TechSolutionMjhgApplication.java
@@ -0,0 +1,13 @@
+package com.epw.tech_solution_mjhg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionMjhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionMjhgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epw/tech_solution_mjhg/TechSolutionMjhgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epw.tech_solution_mjhg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSolutionMjhgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
